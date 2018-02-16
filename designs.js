49fb3fa093e079cb2e1c1d970904514ed3d490e7
Heather You <heatherhyou@gmail.com>
2018-02-16 14:39:50 -0500

    Working pixelmaker

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+$(document).ready(function(){
+  // Put grid into variable
+  var grid = $('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+  // Create grid with user input
+  function makeGrid(height, width){
+    for (var r = 0; r < height; r++) {
+      var row = $('<tr></tr>');
+      for (var c = 0; c < width; c++) {
+        row.append('<td></td>');
+      }
+      grid.append(row);
+    }
+  }
 
-function makeGrid() {
+  $('#submitBtn').on('click', function(e){
+    // Prevent page from refreshing on click
+    e.preventDefault();
+    // Remove previous grid
+    grid.children().remove();
+    // Grab user input
+    var height = $('#inputHeight').val();
+    var width = $('#inputWeight').val();
+    // Call makeGrid function
+    makeGrid(height, width);
+  })
 
-// Your code goes here!
-
-}
+  grid.on('click', 'td', function(){
+    $(this).css('background-color', $('#colorPicker').val())
+  })
+})
